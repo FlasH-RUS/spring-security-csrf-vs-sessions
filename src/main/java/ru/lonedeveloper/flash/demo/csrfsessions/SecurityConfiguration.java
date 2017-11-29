34517eba326fedc7c4246c68bd6f60081651f30e
Vsevolod Kalinin <kalininvsevolod@gmail.com>
2017-11-29 12:56:55 +0200

    CsrfAuthenticationStrategy solved the problem

--- src/main/java/ru/lonedeveloper/flash/demo/csrfsessions/SecurityConfiguration.java
+++ src/main/java/ru/lonedeveloper/flash/demo/csrfsessions/SecurityConfiguration.java
@@ -6,7 +6,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfAuthenticationStrategy;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -23,8 +26,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         super.configure(http);
-        http.csrf().csrfTokenRepository(new CookieCsrfTokenRepository());
+
+        final CsrfTokenRepository csrfTokenRepository = new CookieCsrfTokenRepository();
+
+        http.csrf().csrfTokenRepository(csrfTokenRepository);
         http.sessionManagement().disable();
+        /*
+         * Add CsrfAuthenticationStrategy to plug into AbstractAuthenticationProcessingFilter which is not done by
+         * CsrfConfigurer when session management is disabled. See
+         * https://docs.spring.io/spring-security/site/docs/current/reference/html/session-mgmt.html#
+         * sessionauthenticationstrategy
+         */
+        http.setSharedObject(SessionAuthenticationStrategy.class, new CsrfAuthenticationStrategy(csrfTokenRepository));
     }
 
     @Override
